0693e5996c07d482843e506a2d9b2106cec9c886
CeS-3 <1540962271@qq.com>
2024-07-13 15:21:16 +0800

    确定基本框架

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,181 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <beautiful-chat
+      :always-scroll-to-bottom="alwaysScrollToBottom"
+      :close="closeChat"
+      :colors="colors"
+      :is-open="isChatOpen"
+      :message-list="messageList"
+      :message-styling="messageStyling"
+      :new-messages-count="newMessagesCount"
+      :on-message-was-sent="onMessageWasSent"
+      :onUserInputSubmit="onUserInputSubmit"
+      :open="openChat"
+      :participants="participants"
+      :show-close-button="true"
+      :show-launcher="true"
+      :show-emoji="true"
+      :show-file="true"
+      :show-typing-indicator="showTypingIndicator"
+      :show-edition="true"
+      :show-deletion="true"
+      :show-confirmation-deletion="true"
+      :confirmation-deletion-message="'Are you sure? (you can customize this message)'"
+      :title-image-url="titleImageUrl"
+      :disable-user-list-toggle="false"
+      @onType="handleOnType"
+      @edit="editMessage"
+      @remove="removeMessage"
+    >
+      <template v-slot:header>
+        chat between {{ participants.map((m) => m.name).join(' & ') }}
+      </template>
+      <template v-slot:text-message-toolbox="scopedProps">
+        <button
+          v-if="!scopedProps.me && scopedProps.message.type === 'text'"
+          @click.prevent="like(scopedProps.message.id)"
+        >
+          👍
+        </button>
+      </template>
+      <template v-slot:text-message-body="scopedProps">
+        <p class="sc-message--text-content" v-html="scopedProps.messageText"></p>
+        <p
+          v-if="scopedProps.message.data.meta"
+          class="sc-message--meta"
+          :style="{color: scopedProps.messageColors.color}"
+        >
+          {{ scopedProps.message.data.meta }}
+        </p>
+        <p
+          v-if="scopedProps.message.isEdited || scopedProps.message.liked"
+          class="sc-message--edited"
+        >
+          <template v-if="scopedProps.message.isEdited">✎</template>
+          <template v-if="scopedProps.message.liked">👍</template>
+        </p>
+      </template>
+      <template v-slot:system-message-body="{message}"> [System]: {{ message.text }} </template>
+    </beautiful-chat>
+  <!-- <TestArea
+      :chosen-color="chosenColor"
+      :colors="colors"
+      :message-styling="messageStyling"
+      :on-message="sendMessage"
+      :on-typing="handleTyping"
+    /> -->
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import messageHistory from './components/messageHistory'
+import chatParticipants from './components/chatProfiles'
+// import TestArea from './components/TestArea.vue'
+import availableColors from './components/colors'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // TestArea
+  },
+  data() {
+    return {
+      participants: chatParticipants,
+      titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
+      messageList: messageHistory,
+      newMessagesCount: 0,
+      isChatOpen: false,
+      showTypingIndicator: '',
+      colors: null,
+      availableColors,
+      chosenColor: null,
+      alwaysScrollToBottom: true,
+      messageStyling: true,
+      userIsTyping: false
+    }
+  },
+  computed: {
+    linkColor() {
+      return this.chosenColor === 'dark' ? this.colors.sentMessage.text : this.colors.launcher.bg
+    },
+    backgroundColor() {
+      return this.chosenColor === 'dark' ? this.colors.messageList.bg : '#fff'
+    }
+  },
+  created() {
+    this.setColor('blue')
+  },
+  mounted() {
+    this.messageList.forEach((x) => (x.liked = false))
+  },
+  methods: {
+    sendMessage(text) {
+      if (text.length > 0) {
+        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1
+        this.onMessageWasSent({
+          author: 'support',
+          type: 'text',
+          id: Math.random(),
+          data: {text}
+        })
+      }
+    },
+    handleTyping(text) {
+      this.showTypingIndicator =
+        text.length > 0 ? this.participants[this.participants.length - 1].id : ''
+    },
+    onUserInputSubmit(message){
+      message.text
+      //此处调用后端API,将
+      
+    },
+    onMessageWasSent(message) {
+      this.messageList = [...this.messageList, Object.assign({}, message, {id: Math.random()})]
+    },
+    openChat() {
+      this.isChatOpen = true
+      this.newMessagesCount = 0
+    },
+    closeChat() {
+      this.isChatOpen = false
+    },
+    setColor(color) {
+      this.colors = this.availableColors[color]
+      this.chosenColor = color
+    },
+    showStylingInfo() {
+      alert(
+        'You can use *word* to <strong>boldify</strong>, /word/ to <em>emphasize</em>, _word_ to <u>underline</u>, `code` to <code>write = code;</code>, ~this~ to <del>delete</del> and ^sup^ or ¡sub¡ to write <sup>sup</sup> and <sub>sub</sub>'
+      )
+      // this.$modal.show('dialog', {
+      //   title: 'Info',
+      //   text:
+      //     'You can use *word* to <strong>boldify</strong>, /word/ to <em>emphasize</em>, _word_ to <u>underline</u>, `code` to <code>write = code;</code>, ~this~ to <del>delete</del> and ^sup^ or ¡sub¡ to write <sup>sup</sup> and <sub>sub</sub>'
+      // })
+    },
+    messageStylingToggled(e) {
+      this.messageStyling = e.target.checked
+    },
+    handleOnType(e) {
+      this.$event.$emit('onType', e)
+      this.userIsTyping = true
+    },
+    editMessage(message) {
+      const m = this.messageList.find((m) => m.id === message.id)
+      m.isEdited = true
+      m.data.text = message.data.text
+    },
+    removeMessage(message) {
+      const m = this.messageList.find((m) => m.id === message.id)
+      m.type = 'system'
+      m.data.text = 'This message has been removed'
+    },
+    like(id) {
+      const m = this.messageList.findIndex((m) => m.id === id)
+      var msg = this.messageList[m]
+      msg.liked = !msg.liked
+      this.messageList[m] = msg
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import Chat from 'vue3-beautiful-chat'
+const app = createApp(App)
+app.mount('#app')
+app.use(Chat)
